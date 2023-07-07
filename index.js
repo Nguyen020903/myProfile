faa05c682d0292725e9b37e605066e1cdda6348b
Hai Nguyen <s3978275@rmit.edu.vn>
2023-07-07 17:15:27 +0700

    Change HTML file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Optionally, you can add JavaScript functionality here.
 // For example, you could fetch additional data or add interactivity to the page.
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
